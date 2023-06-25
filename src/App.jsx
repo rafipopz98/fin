c903b71149394d7ea69a1e061f77656baad45dcc
rafipopz98 <itsrealrafi@gmail.com>
2023-06-25 16:35:49 +0530

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from './components/Navbar'
-import ScrollToTop from './components/SmoothScrollbar'
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/SmoothScrollbar";
 
 function App() {
-  
-
   return (
     <>
-    <ScrollToTop />
-      <Navbar/>
+      <ScrollToTop />
+      <Navbar />
+      <LandingPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,31 @@
+// import React from 'react'
+import "./LandingPage.css";
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <div className="lpfirst">
+      <div className="imgs">
+      <img src="https://png.pngtree.com/png-clipart/20200527/ourmid/pngtree-office-supplies-cartoon-stationery-green-calculator-png-image_2209437.jpg" alt="" />
+      </div>
+        <div className="smallcir">
+
+        </div>
+ 
+<div className="circle">
+
+</div>
+      </div>
+      <div className="lpsecond">
+        <div className="quote">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>FinCalc</span><br />
+        Numbers that guide your financial growth
+        </div>
+        <div className="btn">
+        <button>calculate</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
